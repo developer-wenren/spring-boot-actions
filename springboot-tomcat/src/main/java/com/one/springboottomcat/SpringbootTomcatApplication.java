996e80a463572bdf3bc0f8de42aaa3f03effd66f
wrcj12138aaa <wrcj12138@163.com>
2018-10-14 14:14:01 +0800

    - 移除无用代码

--- springboot-tomcat/src/main/java/com/one/springboottomcat/SpringbootTomcatApplication.java
+++ springboot-tomcat/src/main/java/com/one/springboottomcat/SpringbootTomcatApplication.java
@@ -1,6 +1,5 @@
 package com.one.springboottomcat;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -20,9 +19,9 @@ public class SpringbootTomcatApplication extends SpringBootServletInitializer  {
 	    return builder.sources(SpringbootTomcatApplication.class);
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootTomcatApplication.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringbootTomcatApplication.class, args);
+//	}
 
 
 	@RequestMapping(value = "/")
